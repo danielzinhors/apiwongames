a198424568b8a97cedb8b1a2d2b2c9f4508c71fd
Daniel Silveira <danielzinhors@gmail.com>
2021-07-29 21:13:32 -0300

    inicio da criaçao do game

--- api/game/services/game.js
+++ api/game/services/game.js
@@ -7,7 +7,7 @@
 const axios = require("axios");
 const slugify = require("slugify");
 
-async function getInfoGame(slug) {
+async function getGameInfo(slug) {
   const jsdom = require("jsdom");
   const { JSDOM } = jsdom;
   const body = await axios.get(`https://www.gog.com/game/${slug}`);
@@ -64,6 +64,40 @@ async function createManyToManyData(products) {
   ]);
 }
 
+async function createGames(products) {
+  await Promise.all(
+    products.map(async (product) => {
+      const item = await getByName(product.title, "game");
+
+      if (!item) {
+        console.info(`Creating: ${product.title}...`);
+
+        const game = await strapi.services.game.create({
+          name: product.title,
+          slug: product.slug.replace(/_/g, "-"),
+          price: product.price.amount,
+          release_date: new Date(
+            Number(product.globalReleaseDate) * 1000
+          ).toISOString(),
+          categories: await Promise.all(
+            product.genres.map((name) => getByName(name, "category"))
+          ),
+          platforms: await Promise.all(
+            product.supportedOperatingSystems.map((name) =>
+              getByName(name, "platform")
+            )
+          ),
+          developers: [await getByName(product.developer, "developer")],
+          publisher: await getByName(product.publisher, "publisher"),
+          ...(await getGameInfo(product.slug)),
+        });
+
+        return game;
+      }
+    })
+  );
+}
+
 module.exports = {
   populate: async (params) => {
     const gogApiUrl = `https://www.gog.com/games/ajax/filtered?mediaType=game&page=1&sort=popularity`;
@@ -71,5 +105,6 @@ module.exports = {
       data: { products },
     } = await axios.get(gogApiUrl);
     await createManyToManyData([products[2], products[3]]);
+    await createGames([products[2], products[3]]);
   },
 };
